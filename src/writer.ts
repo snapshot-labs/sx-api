2baa195e3efd8dd46c62b535468d1669798cd0a8
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-05-16 16:58:55 +0200

    refactor: use Checkpoint ORM

--- src/writer.ts
+++ src/writer.ts
@@ -2,6 +2,7 @@ import { formatUnits } from '@ethersproject/units';
 import { shortStringArrToStr } from '@snapshot-labs/sx/dist/utils/strings';
 import { validateAndParseAddress } from 'starknet';
 import { getJSON, toAddress, getSpaceName, parseTimestamps } from './utils';
+import { Space, Proposal, Vote, User } from '../codegen/models';
 import type { CheckpointWriter } from '@snapshot-labs/checkpoint';
 
 function intSequenceToString(intSequence) {
@@ -21,108 +22,101 @@ export const handleSpaceCreated: CheckpointWriter = async ({
   blockNumber,
   tx,
   event,
-  mysql,
   instance
 }) => {
   if (!event) return;
 
   console.log('Handle space created');
 
-  const item = {
-    id: validateAndParseAddress(event.space_address),
-    name: getSpaceName(event.space_address),
-    about: '',
-    external_url: '',
-    github: '',
-    twitter: '',
-    discord: '',
-    wallet: '',
-    controller: validateAndParseAddress(event.controller),
-    voting_delay: BigInt(event.voting_delay).toString(),
-    min_voting_period: BigInt(event.min_voting_duration).toString(),
-    max_voting_period: BigInt(event.max_voting_duration).toString(),
-    proposal_threshold: uint256toString(event.proposal_threshold),
-    quorum: uint256toString(event.quorum),
-    strategies: JSON.stringify(event.voting_strategies),
-    strategies_params: JSON.stringify(event.voting_strategy_params_flat),
-    strategies_metadata: JSON.stringify([]),
-    authenticators: JSON.stringify(event.authenticators),
-    executors: JSON.stringify(event.execution_strategies),
-    proposal_count: 0,
-    vote_count: 0,
-    created: block?.timestamp ?? Date.now(),
-    tx: tx.transaction_hash
-  };
+  const space = new Space(validateAndParseAddress(event.space_address));
+  space.name = getSpaceName(event.space_address);
+  space.controller = validateAndParseAddress(event.controller);
+  space.voting_delay = parseInt(event.voting_delay);
+  space.min_voting_period = parseInt(event.min_voting_duration);
+  space.max_voting_period = parseInt(event.max_voting_duration);
+  space.proposal_threshold = parseInt(uint256toString(event.proposal_threshold));
+  space.quorum = parseInt(uint256toString(event.quorum));
+  space.strategies = event.voting_strategies;
+  space.strategies_params = event.voting_strategy_params_flat;
+  space.strategies_metadata = [];
+  space.authenticators = event.authenticators;
+  space.executors = event.execution_strategies;
+  space.proposal_count = 0;
+  space.vote_count = 0;
+  space.created = block?.timestamp ?? Date.now();
+  space.tx = tx.transaction_hash ?? null;
 
   try {
     const metadataUri = shortStringArrToStr(event.metadata_uri || []).replaceAll('\x00', '');
     const metadata: any = metadataUri ? await getJSON(metadataUri) : {};
 
-    if (metadata.name) item.name = metadata.name;
-    if (metadata.description) item.about = metadata.description;
-    if (metadata.external_url) item.external_url = metadata.external_url;
+    if (metadata.name) space.name = metadata.name;
+    if (metadata.description) space.about = metadata.description;
+    if (metadata.external_url) space.external_url = metadata.external_url;
 
     if (metadata.properties) {
-      if (metadata.properties.github) item.github = metadata.properties.github;
-      if (metadata.properties.twitter) item.twitter = metadata.properties.twitter;
-      if (metadata.properties.discord) item.discord = metadata.properties.discord;
+      if (metadata.properties.github) space.github = metadata.properties.github;
+      if (metadata.properties.twitter) space.twitter = metadata.properties.twitter;
+      if (metadata.properties.discord) space.discord = metadata.properties.discord;
       if (metadata.properties.wallets && metadata.properties.wallets.length > 0) {
-        item.wallet = metadata.properties.wallets[0];
+        space.wallet = metadata.properties.wallets[0];
       }
     }
   } catch (e) {
     console.log('failed to parse space metadata', e);
   }
 
+  await space.save();
+
   instance.executeTemplate('Space', {
-    contract: item.id,
+    contract: space.id,
     start: blockNumber
   });
-
-  const query = `INSERT IGNORE INTO spaces SET ?;`;
-  await mysql.queryAsync(query, [item]);
 };
 
-export const handleMetadataUriUpdated: CheckpointWriter = async ({ rawEvent, event, mysql }) => {
+export const handleMetadataUriUpdated: CheckpointWriter = async ({ rawEvent, event }) => {
   if (!event || !rawEvent) return;
 
   console.log('Handle space metadata uri updated');
 
-  const space = validateAndParseAddress(rawEvent.from_address);
+  const spaceAddress = validateAndParseAddress(rawEvent.from_address);
+
+  const space = await Space.loadEntity(spaceAddress);
+  if (!space) return;
 
   try {
     const metadataUri = shortStringArrToStr(event.new_metadata_uri).replaceAll('\x00', '');
     const metadata: any = await getJSON(metadataUri);
 
-    const query = `UPDATE spaces SET name = ?, about = ?, external_url = ?, github = ?, twitter = ?, discord = ?, wallet = ? WHERE id = ? LIMIT 1;`;
-    await mysql.queryAsync(query, [
-      metadata.name,
-      metadata.description,
-      metadata.external_url,
-      metadata.properties?.github,
-      metadata.properties?.twitter,
-      metadata.properties?.discord,
-      metadata.properties?.wallets && metadata.properties?.wallets.length > 0
-        ? metadata.properties?.wallets[0]
-        : '',
-      space
-    ]);
+    if (metadata.name) space.name = metadata.name;
+    if (metadata.description) space.about = metadata.description;
+    if (metadata.external_url) space.external_url = metadata.external_url;
+
+    if (metadata.properties) {
+      if (metadata.properties.github) space.github = metadata.properties.github;
+      if (metadata.properties.twitter) space.twitter = metadata.properties.twitter;
+      if (metadata.properties.discord) space.discord = metadata.properties.discord;
+      if (metadata.properties.wallets && metadata.properties.wallets.length > 0) {
+        space.wallet = metadata.properties.wallets[0];
+      }
+    }
+
+    await space.save();
   } catch (e) {
     console.log('failed to update space metadata', e);
   }
 };
 
-export const handlePropose: CheckpointWriter = async ({ block, tx, rawEvent, event, mysql }) => {
+export const handlePropose: CheckpointWriter = async ({ block, tx, rawEvent, event }) => {
   if (!rawEvent || !event) return;
 
   console.log('Handle propose');
 
-  const space = validateAndParseAddress(rawEvent.from_address);
-  const [{ strategies, strategies_params }] = await mysql.queryAsync(
-    'SELECT strategies, strategies_params FROM spaces WHERE id = ? LIMIT 1',
-    [space]
-  );
-  const proposal = parseInt(BigInt(event.proposal_id).toString());
+  const spaceId = validateAndParseAddress(rawEvent.from_address);
+  const space = await Space.loadEntity(spaceId);
+  if (!space) return;
+
+  const proposalId = parseInt(BigInt(event.proposal_id).toString());
   const author = toAddress(event.proposer_address.value);
   let title = '';
   let body = '';
@@ -147,95 +141,86 @@ export const handlePropose: CheckpointWriter = async ({ block, tx, rawEvent, eve
 
   const created = block?.timestamp ?? Date.now();
 
-  const item = {
-    id: `${space}/${proposal}`,
-    proposal_id: proposal,
-    space,
-    author,
-    execution_hash: event.proposal.execution_hash,
-    metadata_uri: metadataUri,
-    title,
-    body,
-    discussion,
-    execution,
-    start: parseInt(BigInt(timestamps.start).toString()),
-    min_end: parseInt(BigInt(timestamps.minEnd).toString()),
-    max_end: parseInt(BigInt(timestamps.maxEnd).toString()),
-    snapshot: parseInt(BigInt(timestamps.snapshot).toString()),
-    scores_1: 0,
-    scores_2: 0,
-    scores_3: 0,
-    scores_total: 0,
-    quorum: uint256toString(event.proposal.quorum),
-    strategies,
-    strategies_params,
-    created,
-    tx: tx.transaction_hash,
-    vote_count: 0
-  };
-  console.log('Proposal', item);
+  const proposal = new Proposal(`${spaceId}/${proposalId}`);
+  proposal.proposal_id = proposalId;
+  proposal.space = spaceId;
+  proposal.author = author;
+  proposal.execution_hash = event.proposal.execution_hash;
+  proposal.metadata_uri = metadataUri;
+  proposal.title = title;
+  proposal.body = body;
+  proposal.discussion = discussion;
+  proposal.execution = execution;
+  proposal.start = parseInt(BigInt(timestamps.start).toString());
+  proposal.min_end = parseInt(BigInt(timestamps.minEnd).toString());
+  proposal.max_end = parseInt(BigInt(timestamps.maxEnd).toString());
+  proposal.snapshot = parseInt(BigInt(timestamps.snapshot).toString());
 
-  const user = {
-    id: author,
-    vote_count: 0,
-    proposal_count: 0,
-    created
-  };
+  proposal.scores_1 = 0;
+  proposal.scores_2 = 0;
+  proposal.scores_3 = 0;
+  proposal.scores_total = 0;
+  proposal.quorum = parseInt(uint256toString(event.proposal.quorum));
+  proposal.strategies = space.strategies;
+  proposal.strategies_params = space.strategies_params;
+  proposal.created = created;
+  proposal.tx = tx.transaction_hash ?? null;
+  proposal.vote_count = 0;
 
-  const query = `
-    INSERT IGNORE INTO proposals SET ?;
-    UPDATE spaces SET proposal_count = proposal_count + 1 WHERE id = ? LIMIT 1;
-    INSERT IGNORE INTO users SET ?;
-    UPDATE users SET proposal_count = proposal_count + 1 WHERE id = ? LIMIT 1;
-  `;
-  await mysql.queryAsync(query, [item, item.space, user, author]);
+  await proposal.save();
+
+  space.proposal_count = space.proposal_count + 1;
+  await space.save();
+
+  let user = await User.loadEntity(author);
+  if (!user) user = new User(author);
+
+  user.proposal_count = user.proposal_count + 1;
+  user.created = created;
+  await user.save();
 };
 
-export const handleVote: CheckpointWriter = async ({ block, rawEvent, event, mysql }) => {
+export const handleVote: CheckpointWriter = async ({ block, rawEvent, event }) => {
   if (!rawEvent || !event) return;
 
   console.log('Handle vote', event);
 
-  const space = validateAndParseAddress(rawEvent.from_address);
-  const proposal = parseInt(event.proposal_id);
-  const voter = toAddress(event.voter_address.value);
+  const spaceId = validateAndParseAddress(rawEvent.from_address);
+  const proposalid = parseInt(event.proposal_id);
+  const voterId = toAddress(event.voter_address.value);
   const choice = parseInt(BigInt(event.vote.choice).toString());
   const vp = parseFloat(formatUnits(uint256toString(event.vote.voting_power), 18));
 
   const created = block?.timestamp ?? Date.now();
 
-  const item = {
-    id: `${space}/${proposal}/${voter}`,
-    space,
-    proposal,
-    voter,
-    choice,
-    vp,
-    created
-  };
-  console.log('Vote', item);
+  const space = await Space.loadEntity(spaceId);
+  if (space) {
+    space.vote_count = space.vote_count + 1;
+    await space.save();
+  }
 
-  const user = {
-    id: voter,
-    vote_count: 0,
-    proposal_count: 0,
-    created
-  };
+  const proposal = await Proposal.loadEntity(`${spaceId}/${proposalid}`);
+  if (proposal) {
+    proposal.vote_count = proposal.vote_count + 1;
+    proposal.scores_total = proposal.scores_total + vp;
+    proposal.scores_1 = proposal.scores_1 + (choice === 1 ? vp : 0);
+    proposal.scores_2 = proposal.scores_2 + (choice === 2 ? vp : 0);
+    proposal.scores_3 = proposal.scores_3 + (choice === 3 ? vp : 0);
+    await proposal.save();
+  }
 
-  const query = `
-    INSERT IGNORE INTO votes SET ?;
-    UPDATE spaces SET vote_count = vote_count + 1 WHERE id = ? LIMIT 1;
-    UPDATE proposals SET vote_count = vote_count + 1, scores_total = scores_total + ?, scores_${item.choice} = scores_${item.choice} + ? WHERE id = ? LIMIT 1;
-    INSERT IGNORE INTO users SET ?;
-    UPDATE users SET vote_count = vote_count + 1 WHERE id = ? LIMIT 1;
-  `;
-  await mysql.queryAsync(query, [
-    item,
-    item.space,
-    item.vp,
-    item.vp,
-    `${item.space}/${item.proposal}`,
-    user,
-    voter
-  ]);
+  let user = await User.loadEntity(voterId);
+  if (!user) user = new User(voterId);
+  user.created = created;
+  user.vote_count = user.vote_count + 1;
+  await user.save();
+
+  const vote = new Vote(`${spaceId}/${proposalid}/${voterId}`);
+  vote.space = spaceId;
+  vote.proposal = proposalid;
+  vote.voter = voterId;
+  vote.choice = choice;
+  vote.vp = vp;
+  vote.created = created;
+  await vote.save();
 };
